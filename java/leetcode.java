cee267cbfcca74ce4565edd3238a152cd8e73e15
Arseniy Barashko <94114454+SISHRAK@users.noreply.github.com>
2023-11-16 22:15:55 +0300

    Update leetcode.java

--- java/leetcode.java
+++ java/leetcode.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 //  Definition for a binary tree node.
 class TreeNode {
@@ -111,43 +110,45 @@ class Solution {
         int right = maxDepth(root.right);
         return Math.max(left, right) + 1;
     }
+
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int ans = 0;
-        for(int i = 0; i < arr1.length;i++){
-            for(int j = 0; j < arr2.length;j++){
-                if(Math.abs(arr1[i] - arr2[j]) <= d){
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (Math.abs(arr1[i] - arr2[j]) <= d) {
                     ans++;
                 }
             }
         }
         return arr1.length - ans;
     }
+
     public int countNumbersWithUniqueDigits(int n) {
-        if (n == 0){
+        if (n == 0) {
             return 1;
         }
-        if(n == 1){
+        if (n == 1) {
             return 10;
         }
         return (int) Math.pow(10, n) - 9;
     }
+
     public boolean increasingTriplet(int[] nums) {
         int a = Integer.MAX_VALUE;
         int b = Integer.MAX_VALUE;
-        for(int i = 0; i < nums.length;i++){
-            if(nums[i] <= a){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= a) {
                 a = nums[i];
-            }
-            else if(nums[i] < b){
+            } else if (nums[i] < b) {
                 b = nums[i];
-            }
-            else if(nums[i] > b){
+            } else if (nums[i] > b) {
                 return true;
             }
         }
         return false;
     }
-//    public int lengthOfLIS(int[] nums) {
+
+    //    public int lengthOfLIS(int[] nums) {
 //        int ans = 0;
 //        int buf = 0;
 //        for(int i = 0; i < nums.length;i++){
@@ -160,30 +161,228 @@ class Solution {
 //    }
     public int findDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            if(set.contains(num)){
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return num;
             }
             set.add(num);
         }
         return -1;
     }
+
     public int integerBreak(int n) {
-        if(n <= 1){
+        if (n <= 1) {
             return 0;
         }
         if (n == 2) {
             return 1;
         }
-        if(n == 3){
+        if (n == 3) {
             return 2;
         }
         int ans = 1;
-        while(n > 4){
+        while (n > 4) {
             ans *= 3;
             n -= 3;
         }
         ans *= n;
+        return ans;
+    }
+
+    public int minOperations(String[] logs) {
+        List<String> f = new ArrayList<>();
+        for (int i = 0; i < logs.length; i++) {
+            if (logs[i].equals("./")) {
+                continue;
+            } else if (logs[i].equals("../")) {
+                if (f.size() > 1) {
+                    f.remove(f.size() - 1);
+                } else {
+                    f.clear();
+                }
+            } else {
+                f.add(logs[i]);
+            }
+        }
+        return f.size();
+    }
+
+    public int getMaximumGenerated(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int[] a = new int[n + 1];
+        a[0] = 0;
+        a[1] = 1;
+        int max = 0;
+        for (int i = 2; i < n + 1; i++) {
+            if (i % 2 == 0) {
+                a[i] = a[i / 2];
+            } else {
+                a[i] = a[i / 2 + 1] + a[i / 2];
+            }
+            max = Math.max(max, a[i]);
+        }
+        return max;
+    }
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String a = "";
+        String b = "";
+        for (int i = 0; i < word1.length; i++) {
+            a += word1[i];
+        }
+        for (int i = 0; i < word2.length; i++) {
+            b += word2[i];
+        }
+        if (Arrays.equals(a.toCharArray(), b.toCharArray())) {
+            return true;
+        }
+        return false;
+    }
+
+    public int maximumScore(int[] nums, int k) {
+        int res = nums[k], mini = nums[k], i = k, j = k, n = nums.length;
+        while (i > 0 || j < n - 1) {
+            if (i == 0) {
+                j++;
+            } else if (j == n - 1) {
+                i--;
+            } else if (nums[i - 1] < nums[j + 1]) {
+                j++;
+            } else {
+                i--;
+            }
+            mini = Math.min(mini, Math.min(nums[i], nums[j]));
+            res = Math.max(res, mini * (j - i + 1));
+        }
+        return res;
+    }
+
+    public int xorBeauty(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+
+    public int minDistance(int[] houses, int k) {
+        Arrays.sort(houses);
+        int n = houses.length;
+        int[] dp = new int[n];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + houses[i] - houses[i / 2];
+        }
+        for (int i = 0; i < k - 1; i++) {
+            int[] next = new int[n];
+            for (int j = 0; j < next.length; j++) {
+                next[j] = Integer.MAX_VALUE;
+            }
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                for (int m = j; m >= 0; m--) {
+                    sum += houses[(m + j + 1) >> 1] - houses[m];
+                    int buf;
+                    if (m == 0) {
+                        buf = 0;
+                    } else {
+                        buf = dp[m - 1];
+                    }
+                    next[j] = Math.min(next[j], buf + sum);
+                }
+            }
+            dp = next;
+        }
+        return dp[n - 1];
+    }
+
+    public int[] shortestToChar(String s, char c) {
+        int[] ans = new int[s.length()];
+        char[] aa = s.toCharArray();
+        int buf = 0;
+        int prev = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                prev = 0;
+                ans[i] = 0;
+            } else {
+                ans[i] = ++prev;
+            }
+        }
+        prev = s.length();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                prev = 0;
+                ans[i] = Math.min(ans[i], 0);
+            } else
+                ans[i] = Math.min(ans[i], ++prev);
+        }
+        return ans;
+    }
+
+
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        Map<Integer, List<Integer>> stopToBuses = new HashMap<>();
+        for (int busId = 0; busId < routes.length; busId++) {
+            for (int stop : routes[busId]) {
+                stopToBuses.computeIfAbsent(stop, k -> new ArrayList<>()).add(busId);
+            }
+        }
+        if (!stopToBuses.containsKey(source) || !stopToBuses.containsKey(target)) {
+            return -1;
+        }
+        if (source == target) {
+            return 0;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> busesTaken = new HashSet<>();
+        Set<Integer> stopsVisited = new HashSet<>();
+        int res = 0;
+        queue.offer(source);
+        while (!queue.isEmpty()) {
+            res++;
+            int stopsToProcess = queue.size();
+            for (int i = 0; i < stopsToProcess; i++) {
+                int currentStop = queue.poll();
+                for (int busId : stopToBuses.getOrDefault(currentStop, new ArrayList<>())) {
+                    if (busesTaken.contains(busId)) {
+                        continue;
+                    }
+                    busesTaken.add(busId);
+                    for (int nextStop : routes[busId]) {
+                        if (stopsVisited.contains(nextStop)) {
+                            continue;
+                        }
+                        if (nextStop == target) {
+                            return res;
+                        }
+                        queue.offer(nextStop);
+                        stopsVisited.add(nextStop);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int numMatchingSubseq(String s, String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        for (String word : map.keySet()) {
+            int i = 0, j = 0;
+            while (i < s.length() && j < word.length()) {
+                if (s.charAt(i) == word.charAt(j)) {
+                    j++;
+                }
+                i++;
+            }
+            if (j == word.length()) {
+                ans += map.get(word);
+            }
+        }
         return ans;
     }
 }
