0daae5e3fee1aa53223923bdf31229a690f8c942
Arseniy Barashko <94114454+SISHRAK@users.noreply.github.com>
2023-11-18 16:35:54 +0300

    Update leetcode.py

--- Python/leetcode.py
+++ Python/leetcode.py
@@ -1,1 +1,19 @@
-TODO
+from itertools import *
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        def F(num):
+            bit_count = bin(num).count('1')
+            return (bit_count, num)
+        arr.sort(key = F)
+        return arr  
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        ans = 1
+        temp = 1
+        if(n == 1):
+            return 10
+        if(n == 0):
+            return 1
+        for i in range (1, n+1):
+            ans = 9*temp + ans
+            temp = temp * (10 - i)
+        return ans   
